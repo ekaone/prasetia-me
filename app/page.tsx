106e2379eaa9fdff9b5e3a4f071dff042dfac9e3
Eka Prasetia <ekaone3033@gmail.com>
2025-04-30 11:02:38 +0700

    add center screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold radio-canada-big">Personal website</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold radio-canada-big text-9xl text-cyan-200">
+        Personal website
+      </h1>
     </div>
   );
 }
